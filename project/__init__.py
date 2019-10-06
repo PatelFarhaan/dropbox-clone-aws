53c8add67d6cebb6402b44401adf4b3df23dc2a2
Darshan <darshan@DESKTOP-RJ5PBGI.localdomain>
2019-10-06 02:38:34 -0700

    staging remaining

--- project/__init__.py
+++ project/__init__.py
@@ -8,7 +8,7 @@ from sqlalchemy_utils import create_database, database_exists
 ##########################DATABASE##################################
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:***REMOVED_PASSWORD***@localhost/putbox'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:***REMOVED_PASSWORD***@localhost/putbox'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 db = SQLAlchemy(app)
@@ -20,17 +20,16 @@ if not database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
+# login_manager.login_view = 'admin.admin_login'
 
 
 ##############################BLUEPRINTS################################
 from project.core.views import core_blueprint
 from project.users.views import users_blueprint
 from project.admin.views import admin_blueprint
-# from project.blog_posts.views import blogpost_blueprint
 from project.error_pages.handler import error_page_blueprint
 
 app.register_blueprint(core_blueprint)
 app.register_blueprint(admin_blueprint)
 app.register_blueprint(users_blueprint)
-# app.register_blueprint(blogpost_blueprint)
 app.register_blueprint(error_page_blueprint)
---
+++ project/users/models.py
@@ -0,0 +1,42 @@
+import datetime
+from flask_login import UserMixin
+from project import db, login_manager
+
+
+########################################################################################################################
+###############################################* LOGIN MANAGER *########################################################
+########################################################################################################################
+@login_manager.user_loader
+def user_load(user_id):
+    return Users.query.get(int(user_id))
+########################################################################################################################
+
+
+########################################################################################################################
+#####################################################* USERS *##########################################################
+########################################################################################################################
+class Users(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String(64))
+    hashed_password = db.Column(db.String(256))
+    mobile_number = db.Column(db.String(12), nullable=False)
+    email = db.Column(db.String(128), unique=True, index=True)
+    register_date = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
+    storage = db.relationship('Storage', backref='users', lazy=True)
+########################################################################################################################
+
+
+########################################################################################################################
+#################################################* STORAGE *############################################################
+########################################################################################################################
+class Storage(db.Model):
+    __tablename__ = 'storage'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    file = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    filename = db.Column(db.String(1000), nullable=False, unique=True)
+########################################################################################################################
