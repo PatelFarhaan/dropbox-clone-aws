d9cf31879f3518288173d27edb46ee2cd816545a
Farhaan Patel <patel.farhaaan@gmail.com>
2021-04-28 20:49:07 -0700

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import create_database, database_exists
 
 
-###########################DATABASE##################################
+##########################DATABASE##################################
 file_path = os.getcwd() + '/tmp/'
 if not os.path.exists(file_path):
     os.mkdir(file_path)
